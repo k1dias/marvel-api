a1d49220fa047a6f360c8401598c9a8ba8e639eb
Caua Dias <cauadiaspessoal@gmail.com>
2023-02-01 15:15:51 -0300

    remove sensitive data

--- src/marvel.js
+++ src/marvel.js
@@ -1,7 +1,3 @@
-// export const secret_key = "fe1bddf60bda4f5d728873c8c14741e397bd20b9"
-// export const public_key = "eeb02aa91e28f491cc9898855bdb02d4"
-
-
 export const public_key =process.env.VUE_APP_PUBLIC_KEY
 export const secret_key =process.env.VUE_APP_SECRET_KEY
 
